b1036df57fdc93caadb35366ea27c91582f761de
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2021-06-01 20:22:46 +0300

    module2-task1 Превел функции к стрелочному виду

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
-function randomNumber(minValue, maxValue) {
-  if (minValue >= 0 && maxValue >= 0) {
+const randomNumber = (minValue, maxValue) => {
+  if (minValue >=0 && maxValue >=0) {
     minValue = Math.ceil(minValue);
     maxValue = Math.floor(maxValue);
     return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   } else {
     return ('Диапазон может быть только положительный, включая ноль.');
   }
-}
+};
 randomNumber();
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 // Будет использоваться для генерации временных географических координат в следующем задании.
-function geographicalCoordinates(latitude, longitude, TO_FIXED) {
+const geographicalCoordinates = (latitude, longitude, toFloat) => {
   if (latitude >= 0 && longitude >= 0 && latitude < longitude) {
-    return ((Math.random() * (longitude - latitude + 1)) + latitude).toFixed(TO_FIXED);
+    return ((Math.random() * (longitude - latitude + 1)) + latitude).toFixed(toFloat);
   } else {
     return ('Данные некорректны, перечитайте задание.');
   }
-}
+};
 geographicalCoordinates();
