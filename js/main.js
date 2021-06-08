ad68de55a4ae0d29f8f9931dac80808b08920a24
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2021-06-08 20:24:34 +0300

    Module4-task1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+//  Все переменные - константы
+const title = ['Лучшее предложение в этом районе!', 'Сдается шикарная, просторная квартира в новом, элитном доме', 'Сдается на длительный срок квартира в самом центре столицы', 'Два уровня комфорта в центре'];
+// const address = {{location.x}}, {{location.y}};
+const minPrice = 100;
+const maxPrice = 100000;
+const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const minNumberOfRooms = 1;
+const maxNumberOfRooms = 6;
+const minNumberOfGuests = 1;
+const maxNumberOfGuests = 12;
+const checkin = ['12:00', '13:00', '14:00'];
+const checkout = ['12:00', '13:00', '14:00'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const description = [' сделан хороший ремонт', 'сдам квартиру семейной паре', 'без животных'];
+const photo = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const minLatitude = 35.6500;
+const maxLatitude = 35.7000;
+const minLongitude = 139.7500;
+const maxLongitude = 139.8000;
+
+
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const randomNumber = (minValue, maxValue) => {
   if (minValue >=0 && maxValue >=0) {
@@ -18,3 +43,30 @@ const geographicalCoordinates = (latitude, longitude, toFloat) => {
   return ('Данные некорректны, перечитайте задание.');
 };
 geographicalCoordinates();
+
+// Функция, возвращающая случайный элемент из массива
+function randomArrayElement(array) {
+  return array[randomNumber(0, array.length - 1)];
+}
+
+const createAnAdvertisement = {
+  'author': {
+    avatar: `img/avatars/user0${  randomNumber(1, 8)  }.png`,
+  },
+  'offer': {
+    'title': randomArrayElement(title),
+    'price': randomNumber(minPrice, maxPrice),
+    'type': randomArrayElement(type),
+    'rooms': randomNumber(minNumberOfRooms, maxNumberOfRooms),
+    'guests': randomNumber(minNumberOfGuests, maxNumberOfGuests),
+    'checkin':randomArrayElement(checkin),
+    'checkout':randomArrayElement(checkout),
+    'features': randomArrayElement(features),
+    'description': randomArrayElement(description),
+    'photo': randomArrayElement(photo),
+  },
+  'location': {
+    'lat': geographicalCoordinates(minLatitude, maxLatitude, 4),
+    'lng': geographicalCoordinates(minLongitude, maxLongitude, 4),
+  },
+};
