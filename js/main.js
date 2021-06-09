23b2a2733e08fe75463e30cd4b8c7298f0a7aff3
stanislauhvozdz <66819873+stanislauhvozdz@users.noreply.github.com>
2021-06-10 00:00:41 +0300

    исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 //  Все переменные - константы
-const title = ['Лучшее предложение в этом районе!', 'Сдается шикарная, просторная квартира в новом, элитном доме', 'Сдается на длительный срок квартира в самом центре столицы', 'Два уровня комфорта в центре'];
-// const address = {{location.x}}, {{location.y}};
-const minPrice = 100;
-const maxPrice = 100000;
+const titles = ['Лучшее предложение в этом районе!', 'Сдается шикарная, просторная квартира в новом, элитном доме', 'Сдается на длительный срок квартира в самом центре столицы', 'Два уровня комфорта в центре'];
+const MIN_PRICE = 100;
+const MAX_PRICE = 100000;
 const type = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const minNumberOfRooms = 1;
-const maxNumberOfRooms = 6;
-const minNumberOfGuests = 1;
-const maxNumberOfGuests = 12;
-const checkin = ['12:00', '13:00', '14:00'];
-const checkout = ['12:00', '13:00', '14:00'];
+const MIN_NUMBERS_OF_ROOMS = 1;
+const MAX_NUMBERS_OF_ROOMS = 6;
+const MIN_NUMBER_OF_GUESTS = 1;
+const MAX_NUMBER_OF_GUESTS = 12;
+const TIMES = {
+  checkin: ['12:00', '13:00', '14:00'],
+  checkout: ['12:00', '13:00', '14:00'],
+};
 const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const description = [' сделан хороший ремонт', 'сдам квартиру семейной паре', 'без животных'];
 const photo = [
@@ -17,14 +18,14 @@ const photo = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
-const minLatitude = 35.6500;
-const maxLatitude = 35.7000;
-const minLongitude = 139.7500;
-const maxLongitude = 139.8000;
+const MIN_LATITUDE = 35.6500;
+const MAX_LATITUDE = 35.7000;
+const MIN_LONGITUDE = 139.7500;
+const MAX_LONGITUDE = 139.8000;
 
 
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
-const randomNumber = (minValue, maxValue) => {
+const getRandomNumber = (minValue, maxValue) => {
   if (minValue >=0 && maxValue >=0) {
     minValue = Math.ceil(minValue);
     maxValue = Math.floor(maxValue);
@@ -32,41 +33,45 @@ const randomNumber = (minValue, maxValue) => {
   }
   return ('Диапазон может быть только положительный, включая ноль.');
 };
-randomNumber();
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 // Будет использоваться для генерации временных географических координат в следующем задании.
-const geographicalCoordinates = (latitude, longitude, toFloat) => {
+const getGeographicalCoordinates = (latitude, longitude, toFloat) => {
   if (latitude >= 0 && longitude >= 0 && latitude < longitude) {
     return ((Math.random() * (longitude - latitude + 1)) + latitude).toFixed(toFloat);
   }
   return ('Данные некорректны, перечитайте задание.');
 };
-geographicalCoordinates();
 
 // Функция, возвращающая случайный элемент из массива
-function randomArrayElement(array) {
-  return array[randomNumber(0, array.length - 1)];
-}
+const getRandomArrayElement = (array) => array[getRandomNumber(0, array.length - 1)];
+
+const LAT = getGeographicalCoordinates(MIN_LATITUDE, MAX_LATITUDE, 4);
+const LNG = getGeographicalCoordinates(MIN_LONGITUDE, MAX_LONGITUDE, 4);
 
 const createAnAdvertisement = {
   'author': {
-    avatar: `img/avatars/user0${  randomNumber(1, 8)  }.png`,
+    avatar: `img/avatars/user0${getRandomNumber(1, 8)}.png`,
   },
   'offer': {
-    'title': randomArrayElement(title),
-    'price': randomNumber(minPrice, maxPrice),
-    'type': randomArrayElement(type),
-    'rooms': randomNumber(minNumberOfRooms, maxNumberOfRooms),
-    'guests': randomNumber(minNumberOfGuests, maxNumberOfGuests),
-    'checkin':randomArrayElement(checkin),
-    'checkout':randomArrayElement(checkout),
-    'features': randomArrayElement(features),
-    'description': randomArrayElement(description),
-    'photo': randomArrayElement(photo),
+    'title': getRandomArrayElement(titles),
+    'adress': `location.x = ${LAT}, location.y = ${LNG}`,
+    'price': getRandomNumber(MIN_PRICE, MAX_PRICE),
+    'type': getRandomArrayElement(type),
+    'rooms': getRandomNumber(MIN_NUMBERS_OF_ROOMS, MAX_NUMBERS_OF_ROOMS),
+    'guests': getRandomNumber(MIN_NUMBER_OF_GUESTS, MAX_NUMBER_OF_GUESTS),
+    'checkin':getRandomArrayElement(TIMES.checkin),
+    'checkout':getRandomArrayElement(TIMES.checkout),
+    'features': getRandomArrayElement(features),
+    'description': getRandomArrayElement(description),
+    'photo': getRandomArrayElement(photo),
   },
   'location': {
-    'lat': geographicalCoordinates(minLatitude, maxLatitude, 4),
-    'lng': geographicalCoordinates(minLongitude, maxLongitude, 4),
+    'lat': `${LAT}`,
+    'lng': `${LNG}`,
   },
 };
+
+const getGenerateAnArray = new Array(5).fill().map(() => createAnAdvertisement);
+
+console.log(getGenerateAnArray);
